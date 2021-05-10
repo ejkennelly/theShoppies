47e225daac391163fa22ed90517ec0638e898efb
Eliza Kennelly <elizakennelly@gmail.com>
2021-05-09 22:39:52 -0500

    local storage working

--- theshoppies/src/App.js
+++ theshoppies/src/App.js
@@ -1,76 +1,92 @@
-import React, { useState, useEffect } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import './App.css';
-import MovieList from "./components/MovieList";
-import MovieListHeading from "./components/MovieListHeading";
-import SearchBox from "./components/SearchBox";
-import AddNominee from "./components/AddNominee";
-import RemoveNominee from "./components/RemoveNominee";
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import MovieList from './components/MovieList';
+import MovieListHeading from './components/MovieListHeading';
+import SearchBox from './components/SearchBox';
+import AddNominee from './components/AddNominee';
+import RemoveNominee from './components/RemoveNominee';
 
 const App = () => {
-  const [movies, setMovies] = useState([]);
-  const [nominees, setNominees] = useState([]);
-  const [searchValue, setSearchValue] = useState("");
+	const [movies, setMovies] = useState([]);
+	const [nominees, setNominees] = useState([]);
+	const [searchValue, setSearchValue] = useState('');
 
-  const getMovieRequest = async (searchValue) => {
-    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=trilogy`;
+	const getMovieRequest = async (searchValue) => {
+		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=trilogy`;
 
-    const response = await fetch(url);
-    const responseJson = await response.json();
+		const response = await fetch(url);
+		const responseJson = await response.json();
 
-    if (responseJson.Search) {
-      setMovies(responseJson.Search);
-    }
-  };
-  
-  useEffect(() => {
-    getMovieRequest(searchValue);
-  }, [searchValue]);
+		if (responseJson.Search) {
+			setMovies(responseJson.Search);
+		}
+	};
 
-  useEffect(() => {
-    const movieNominees = JSON.parse(
-      localStorage.getItem("the-shoppies-nominees")
-    );
+	useEffect(() => {
+		getMovieRequest(searchValue);
+	}, [searchValue]);
 
-    if (movieNominees) {
-      setNominees(movieNominees);
-    }
-  }, []);
+	useEffect(() => {
+		const movieNominees = JSON.parse(
+			localStorage.getItem("the-shoppies-nominees")
+		);
 
-  const saveToLocalStorage = (items) => {
-    localStorage.setItem("the-shoppies-nominees", JSON.stringify(items));
-  };
+		if (movieNominees) {
+			setNominees(movieNominees);
+		}
+	}, []);
 
-  const addNominee = (movie) => {
-    const newNomineeList = [...nominees, movie];
-    setNominees(newNomineeList);
+	const saveToLocalStorage = (items) => {
+		localStorage.setItem("the-shoppies-nominees", JSON.stringify(items));
+	};
+
+	const addNominees = (movie) => {
+		const newNomineeList = [...nominees, movie];
+		setNominees(newNomineeList);
     saveToLocalStorage(newNomineeList);
-  };
+    if (newNomineeList.length >= 6) {
+      alert("Please remove a nominee before adding another one");
+    
+    };
+	};
 
-  const removeNominee = (movie) => {
-    const newNomineeList = nominees.filter(
-      (nominee) => nominee.imdbID !== movie.imdbID
-    );
+	const removeNominees = (movie) => {
+		const newNomineeList = nominees.filter(
+			(nominees) => nominees.imdbID !== movie.imdbID
+		);
 
-    setNominees(newNomineeList);
-    saveToLocalStorage(newNomineeList)
-  };
+		setNominees(newNomineeList);
+		saveToLocalStorage(newNomineeList);
+	};
 
-  return (
-    <div className="container-fluid movie-app">
-      <div  className="row d-flex align-items-center mt-4 mb-4">
-        <MovieListHeading heading= "Movies" />
-        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
-      </div>
-      <div className="row">
-        <MovieList
-          movies={movies}
-          handleNomineesClick={addNominee}
-          nomineeComponent={RemoveNominee}
-          />
-      </div>
-    </div>
-  );
+  
+	return (
+		<div className='container-fluid movie-app'>
+			<div className='row d-flex align-items-center mt-4 mb-4'>
+				<MovieListHeading heading='The Shoppies' />
+				<SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+			</div>
+			<div className='row'>
+				<MovieList
+					movies={movies}
+					handleNomineesClick={addNominees}
+          nomineeComponent={AddNominee}
+  
+				/>
+			</div>
+			<div className='row d-flex align-items-center mt-4 mb-4'>
+				<MovieListHeading heading='Nominees' />
+			</div>
+			<div className='row d-flex align-items-center mt-4 mb-4'>
+				<MovieList
+					movies={nominees}
+					handleNomineesClick={removeNominees}
+					nomineeComponent={RemoveNominee}
+				/>
+			</div>
+		</div>
+	);
 };
 
 export default App;
--- theshoppies/src/components/MovieList.js
+++ theshoppies/src/components/MovieList.js
@@ -1,18 +1,26 @@
 import React from "react";
 
 const MovieList = (props) => {
-    const NomineeComponent = props.NomineeComponent;
+    const NomineeComponent = props.nomineeComponent;
+    
 
     return (
         <>
             {props.movies.map((movie, index) => (
                 <div className="image-container d-flex justify-content-start m-3">
+                    <figure>
                     <img src={movie.Poster} alt="movie"></img>
+                    <figcaption>{movie.Title}
+                    <br></br>
+                    {movie.Year}
+                    </figcaption>
+                    </figure>
                     <div
-                        onClick={() => props.handleNomineeClick(movie)}
+                        onClick={() => props.handleNomineesClick(movie)}
                         className="overlay d-flex align-items-center justify-content-center"
                     >
                         <NomineeComponent />
+                    
                     </div>
                 </div>
             ))}
