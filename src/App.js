e8e816bf0655a2babf33d32142bdcc7e805be4f5
EJ <70607401+ejkennelly@users.noreply.github.com>
2021-05-11 14:41:02 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
 	const [searchValue, setSearchValue] = useState('');
 
 	const getMovieRequest = async (searchValue) => {
-		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=trilogy`;
+		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=435892ff`;
 
 		const response = await fetch(url);
 		const responseJson = await response.json();
